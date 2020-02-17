a7da57be0ab0f3e4a87e5582d307ca6123f45179
bmann-avamere <43858143+bmannavamere@users.noreply.github.com>
2020-02-17 08:43:30 -0800

    pretty close to finished.

--- app/uploadTheListsToCC.php
+++ app/uploadTheListsToCC.php
@@ -31,69 +31,103 @@ define("ACCESS_TOKEN", "cac50a04-85cb-4d34-ab2f-2a097fe29895");
 
 $cc = new ConstantContact(APIKEY);
 
-// This file queries the database and creates csv files for each cbc
+// This file queries the database and creates csv files for each cbc.
+// Also it creates the $csvFileNamesArr used below.
 require_once './db-csv/createCSVfromDB.php';
 
 // GOALS
 // -----------------------------------------------------------------------------
 // for each .csv file in ../csvContacts upload those contacts to CC
 
-echo '<pre>';
-  print_r($csvFileNamesArr);
-echo '</pre>';
-echo '<hr style="border:2.4px solid black;">';
+// echo '<pre>';
+//   print_r($csvFileNamesArr);
+// echo '</pre>';
+// echo '<hr style="border:2.4px solid black;">';
 
-// Loop through the array of csv file names to do a batch upload to each list
-// note the $csvFileNamesArr comes from createCSVfromDB.php
+// A hard coded array of the contact list ID's from Constant Contact. To find
+// these ID's you have to call to the API (
+// use the API tester -> https://constantcontact.mashery.com/io-docs ) as the
+// ID's are not visibile in the GUI (CC's website).
+// Notice I have named the indexes to match the csv file names, which makes it
+// easy to find a match.
+$listIDsArray = array(
+  // 'avamere-at-albany.csv' => '1548726530',
+  // 'avamere-at-bethany.csv' => '1995041569',
+  // 'avamere-at-cascadia-village.csv' => '1585174354',
+  // 'avamere-at-chestnut-lane.csv' => '1530752912',
+  // 'avamere-at-cheyenne.csv' => '1461093868',
+  // 'avamere-at-englewood-heights.csv' => '1490802359'
+  // ^^^^^^^^^ ABOVE ARE ALL THE TEST LIST ID'S
+  // THE Actual Contact List ID's are below
+  'avamere-at-albany.csv' => '1915856765',
+  'avamere-at-bethany.csv' => '1654718594',
+  'avamere-at-cascadia-village.csv' => '2063850485',
+  'avamere-at-chestnut-lane.csv' => '1874978424',
+  'avamere-at-cheyenne.csv' => '1098034043',
+  'avamere-at-englewood-heights.csv' => '1397474243',
+  'avamere-at-hermiston' => '2060751957',
+  'avamere-at-hillsboro' => '2082235697',
+  'avamere-at-lexington' => '2132841659',
+  'avamere-at-moses-lake' => '1254127753',
+  // 'avamere-at-mountain-ridge' => '',
+  'avamere-at-newberg' => '1593570705',
+  'avamere-at-oak-park' => '1625347543',
+  'avamere-at-park-place' => '1493187826',
+  'avamere-at-port-townsend' => '2009222702',
+  'avamere-at-rio-rancho' => '1758681945',
+  'avamere-at-roswell' => '1567867699',
+  'avamere-at-sandy' => '1577313238',
+  'avamere-at-seaside' => '2040156059',
+  'avamere-at-sherwood' => '1674943066',
+  'avamere-at-south-hill' => '1657041754',
+  'avamere-at-st-helens' => '1813561808',
+  'avamere-at-the-stratford' => '1442358372',
+  'avamere-at-wenatchee' => '1747134508',
+  'avamere-living-at-berry-park' => '1102345200',
+  'suzanne-elise' => '1625361259',
+  'the-arbor-at-bremerton' => '1227904174',
+  'the-stafford' => '1116238271'
+);
+// uncomment below to see the array
+// echo '<pre>';
+// print_r($listIDsArray);
+// echo '</pre>';
+
+// Loop through the array of csv file names to do a batch upload to each list.
+// Note the $csvFileNamesArr comes from createCSVfromDB.php
 foreach ($csvFileNamesArr as $file) {
-  // uncomment to see the list of csv files
-  echo '<pre>';
-    print_r($file);
-  echo '</pre>';
+  echo 'The file to find is: '.$file;
+  echo '<br><hr>';
+  // Here we find a match of the csv file name and the named index in the
+  // $listIDsArray array
+  foreach ($listIDsArray as $key => $value) {
+     // If the file names match
+     if ($file == $key) {
+       echo 'match found for: '.$file.' <br>';
+       echo 'The list ID num is: '.$value;
+       echo ' <hr><br>';
 
-  // !!!!!!! START HERE !!!!!!!!
-  // TEST LIST ID'S IN CC - these will just need to be hardcoded?
-  // /avamere-at-albany 1548726530
-  // /avamere-at-bethany 1995041569
-  // /avamere-at-cascadia-village 1585174354
-  // /avamere-at-chestnut-lane 1530752912
-  // /avamere-at-cheyenne 1461093868
-  // /avamere-at-englewood-heights 1490802359
+       // $fileName = 'testCsvFileToUpload.csv';
+       $fileName = $file;
+       // TEST CBC Exports list ID
+       // $lists = '1661904574';
+       $lists = $value;
 
-  $listIDsArray = array(
-    'avamere-at-albany.csv' => '1548726530',
-    '/avamere-at-bethany.csv' => '1995041569',
-    '/avamere-at-cascadia-village.csv' => '1585174354',
-    '/avamere-at-chestnut-lane.csv' => '1530752912',
-    '/avamere-at-cheyenne.csv' => '1461093868',
-    '/avamere-at-englewood-heights.csv' => '1490802359'
-  );
-  // echo '<pre>';
-  //   print_r($listIDsArray);
-  // echo '</pre>';
+       // $fileLocation = '../fileLocation/cbcContactsExport.csv';
+       // $fileLocation = 'testCsvFileToUpload.csv';
+       $fileLocation = '../csvContacts/'.$file;
 
-  // make an array of list ID's that are matched to the filename (page path)
-  // i.e. avamere-at-albany => List ID 324323
-  // CREATE SEVERAL OR MORE.... 10? whatever might hit the rate limit (4 or more)?
-
+       $fileUploadStatus = $cc->activityService->createAddContactsActivityFromFile(ACCESS_TOKEN, $fileName, $fileLocation, $lists);
+       echo 'Contacts Uploaded... <hr><br>';
+     } else {
+       // echo ' No match found.';
+     }
+  }
 }
 
 
-
-// // ^^^^^ MOVE THIS CODE UP INTO THERE eeeEEEeeeEEEEeeeeeeeeeeeeeee....
-// // if ( some condition then run ) {
-//     $fileName = 'testCsvFileToUpload.csv';
-//     // TEST CBC Exports list ID
-//     $lists = '1661904574';
-//
-//     // $fileLocation = '../fileLocation/cbcContactsExport.csv';
-//     $fileLocation = 'testCsvFileToUpload.csv';
-//
-//     $fileUploadStatus = $cc->activityService->createAddContactsActivityFromFile(ACCESS_TOKEN, $fileName, $fileLocation, $lists);
-//
-// // }
-//
-// // DO I NEED THIS BELOW CODE? WHAT DOES IT DO????????????????????????
+// // This bit of code gets all the contact lists and then they are shown in the
+// // select in the GUI below (if you want to use the GUI).
 // $contactLists = array();
 // $params = array();
 // $listsResult = $cc->listService->getLists(ACCESS_TOKEN, $params);
@@ -102,22 +136,6 @@ foreach ($csvFileNamesArr as $file) {
 // }
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <!-- GUI For manually uploading -->
