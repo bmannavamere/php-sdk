2bc96ab0886ead49a87aa3fb1681c9d8d090c656
bmann-avamere <43858143+bmannavamere@users.noreply.github.com>
2020-03-06 08:03:01 -0800

    I broke the code into 6 different files for 6 different cron jobs, and tested them - no error was thrown, but we will see!

--- app/uploadTheListsToCC.php
+++ app/uploadTheListsToCC.php
@@ -1,24 +1,3 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Push Contacts to Constant Contact</title>
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-    <link href="styles.css" rel="stylesheet">
-</head>
-
-<!--
-README: Import Contacts from file example
-
-This file is a modified version of the sample code found here: https://github.com/constantcontact/php-sdk
-
-To run this file visit: https://avameremarketing.com/pushToConstantContact/app/uploadTheListsToCC.php
-
-It is also run by a cron job once per day.
-
-To learn more read the documentation found on MarCom's 'Share' drive.
-
--->
-
 <?php
 
 ini_set('display_errors', 1);
@@ -60,33 +39,30 @@ $listIDsArray = array(
   'avamere-at-bethany.csv' => '1654718594',
   'avamere-at-cascadia-village.csv' => '2063850485',
   'avamere-at-chestnut-lane.csv' => '1874978424',
-  'avamere-at-cheyenne.csv' => '1098034043',
-  'avamere-at-englewood-heights.csv' => '1397474243',
-  'avamere-at-hermiston.csv' => '2060751957',
-  'avamere-at-hillsboro.csv' => '2082235697',
-  'avamere-at-lexington.csv' => '2132841659',
-  'avamere-at-moses-lake.csv' => '1254127753',
-  // We currently do not control the website for Mountain Ridge and have
-  // therefore received no web forms from it.
-  // 'avamere-at-mountain-ridge' => '',
-  'avamere-at-newberg.csv' => '1593570705',
-  'avamere-at-oak-park.csv' => '1625347543',
-  'avamere-at-park-place.csv' => '1493187826',
-  'avamere-at-port-townsend.csv' => '2009222702',
-  'avamere-at-rio-rancho.csv' => '1758681945',
-  'avamere-at-roswell.csv' => '1567867699',
-  'avamere-at-sandy.csv' => '1577313238',
-  'avamere-at-seaside.csv' => '2040156059',
-  'avamere-at-sherwood.csv' => '1674943066',
-  'avamere-at-south-hill.csv' => '1657041754',
-  'avamere-at-st-helens.csv' => '1813561808',
-  'avamere-at-the-stratford.csv' => '1442358372',
-  'avamere-at-wenatchee.csv' => '1747134508',
-  'avamere-living-at-berry-park.csv' => '1102345200',
-  'suzanne-elise.csv' => '1625361259',
-  'the-arbor-at-avamere-court.csv' => '1290808128',
-  'the-arbor-at-bremerton.csv' => '1227904174',
-  'the-stafford.csv' => '1116238271'
+  'avamere-at-cheyenne.csv' => '1098034043'
+  // 'avamere-at-englewood-heights.csv' => '1397474243',
+  // 'avamere-at-hermiston.csv' => '2060751957',
+  // 'avamere-at-hillsboro.csv' => '2082235697',
+  // 'avamere-at-lexington.csv' => '2132841659',
+  // 'avamere-at-moses-lake.csv' => '1254127753',
+  // 'avamere-at-newberg.csv' => '1593570705',
+  // 'avamere-at-oak-park.csv' => '1625347543',
+  // 'avamere-at-park-place.csv' => '1493187826',
+  // 'avamere-at-port-townsend.csv' => '2009222702',
+  // 'avamere-at-rio-rancho.csv' => '1758681945',
+  // 'avamere-at-roswell.csv' => '1567867699',
+  // 'avamere-at-sandy.csv' => '1577313238',
+  // 'avamere-at-seaside.csv' => '2040156059',
+  // 'avamere-at-sherwood.csv' => '1674943066',
+  // 'avamere-at-south-hill.csv' => '1657041754',
+  // 'avamere-at-st-helens.csv' => '1813561808',
+  // 'avamere-at-the-stratford.csv' => '1442358372',
+  // 'avamere-at-wenatchee.csv' => '1747134508',
+  // 'avamere-living-at-berry-park.csv' => '1102345200',
+  // 'suzanne-elise.csv' => '1625361259',
+  // 'the-arbor-at-avamere-court.csv' => '1290808128',
+  // 'the-arbor-at-bremerton.csv' => '1227904174',
+  // 'the-stafford.csv' => '1116238271'
 );
 
 echo '<pre>';
@@ -137,79 +113,3 @@ foreach ($csvFileNamesArr as $file) {
      }
   }
 }
-
-
-// // This bit of code below gets all the contact lists and then they are shown in the
-// // select in the GUI below (if you want to use the GUI).
-// $contactLists = array();
-// $params = array();
-// $listsResult = $cc->listService->getLists(ACCESS_TOKEN, $params);
-// foreach ($listsResult as $list) {
-//     array_push($contactLists, $list);
-// }
-
-?>
-
-
-<!-- GUI For manually uploading one file at a time - NOT Used -->
-<!-- <body>
-<div class="well">
-    <h3>Import a spreadsheet of Contacts (.xls, .xlsx, .csv, .txt)</h3>
-
-    <form class="form-horizontal" name="submitFile" id="submitFile" method="POST" action="importFromFile.php" enctype="multipart/form-data">
-        <div class="control-group">
-            <label class="control-label" for="file_name">File Name</label>
-
-            <div class="controls">
-                <input type="text" id="file_name" name="file_name" placeholder="File Name">
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="file">File</label>
-
-            <div class="controls">
-                <input type="file" id="file" name="file" placeholder="Choose File">
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="folder">Folder</label>
-
-            <div class="controls">
-                <select multiple name="lists" style="resize: both; overflow: auto;">
-                  <  ?php
-                    foreach ($contactLists as $list) {
-                        echo '<option value="' . $list->id . '">' . $list->name . '</option>';
-                    }
-                    ?>
-                </select>
-
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label">
-                <div class="controls">
-                    <input type="submit" value="Submit" class="btn btn-primary"/>
-                </div>
-        </div>
-    </form>
-</div> -->
-
-<?php
-// Print the details of the contact upload status to screen - NOTE - this only
-// prints the last one that was uploaded.
-// if (isset($fileUploadStatus)) {
-//   echo '<span class="label label-success">File Uploaded!</span>';
-//   echo '<div class="container alert-success"><pre class="success-pre">';
-//
-//           print_r($fileUploadStatus);
-//
-//       echo '</pre></div>';
-//
-//       echo '<pre>';
-//         var_dump($_POST);
-//       echo '</pre>';
-// }
-?>
-
-</body>
-</html>
